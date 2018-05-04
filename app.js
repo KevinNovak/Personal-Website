9dc35b6de523b7eb9d6a337c3565e925226b465f
Kevin Novak <kevinmnovak@yahoo.com>
2018-05-03 23:44:12 -0400

    Converted to hbs view engine

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const hbs = require('hbs');
 const path = require('path');
 
 const httpPort = 3000;
@@ -6,8 +7,19 @@ const httpPort = 3000;
 // App setup
 var app = express();
 
+// Handlebars
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, './public/views'));
+
 // Static files
 app.use(express.static(path.join(__dirname, './public')));
+
+app.get('/', (request, response) => {
+    year = new Date().getFullYear();
+    response.render('index.hbs', {
+        year
+    });
+});
 
 // Start server
 var server = app.listen(httpPort, () => {
